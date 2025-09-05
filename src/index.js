08a31d1d9e3824d4d1caeb5f78043279df158e2d
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-09-05 16:58:58 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-module.exports = function towelSort(matrix) {
-  // const matrix = [
-  //   [ 1, 2, 3 ],
-  //   [ 4, 5, 6 ],
-  //   [ 7, 8, 9 ],
-  //  ]
+module.exports = function towelSort(matrix = []) {
+  // matrix = matrix || undefined;
   const result = [];
+
   for (let i = 0; i < matrix.length; i += 1) {
     for (let j = 0; j < matrix[i].length; j += 1) {
       const towelIndex = i % 2 === 0 ? j : matrix[i].length - j - 1;
       result.push(matrix[i][towelIndex]);
     }
   }
+  // return matrix === undefined ? [] : result;
   return result;
 };
