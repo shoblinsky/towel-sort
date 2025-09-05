8cf44ce637ed048a7e6a69272a30891f559aa561
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-09-05 16:35:22 +0500

    init: start towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  for (let i = 0; i < matrix.length; index++) {
+    for (let j = 0; j < matrix[i].length; index++) {
+
+    }
+
+  }
 };
