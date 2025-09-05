edc0d3764e7385bfc8cd931f3ca4b6720f972911
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-09-05 17:00:22 +0500

    refactor: delete comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 module.exports = function towelSort(matrix = []) {
-  // matrix = matrix || undefined;
   const result = [];
-
   for (let i = 0; i < matrix.length; i += 1) {
     for (let j = 0; j < matrix[i].length; j += 1) {
       const towelIndex = i % 2 === 0 ? j : matrix[i].length - j - 1;
       result.push(matrix[i][towelIndex]);
     }
   }
-  // return matrix === undefined ? [] : result;
   return result;
 };
